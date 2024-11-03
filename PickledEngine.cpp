782dea77c161f1fae1708e59c050a74a72790de6
victorSejourne <victor.sejourne@etu.univ-amu.fr>
2024-11-03 11:53:16 +0100

    feat : seting up ConsoleGameEngine

--- PickledEngine.cpp
+++ PickledEngine.cpp
@@ -1,2 +1,30 @@
 #include "olcConsoleGameEngine.h"
 
+class olcEngine3D : public olcConsoleGameEngine
+{
+public:
+	olcEngine3D()
+	{
+		m_sAppName = L"Pickled Engine";
+	}
+public:
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		return true;
+	}
+};
+
+int main()
+{
+	olcEngine3D engine;
+	if (engine.ConstructConsole(250, 250, 4, 4))
+	{
+		engine.Start();
+	}
+	return 0;
+}
