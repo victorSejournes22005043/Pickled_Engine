1c5337e30b1fab411d98b5039601581df3e24d47
victorSejournes22005043 <victor.sejourne@etu.univ-amu.fr>
2024-11-25 19:24:11 +0100

    wip : triangle class

--- PickledEngine.cpp
+++ PickledEngine.cpp
@@ -1,5 +1,36 @@
 #include "olcConsoleGameEngine.h"
 
+struct vec2d 
+{
+	float x, y;
+};
+
+class triangle
+{
+public:
+	triangle()
+	{
+		vec2d coordA;
+		vec2d coordB;
+		vec2d coordC;
+
+		coordA.x = 0.0f;
+		coordA.y = 0.0f;
+
+		coordB.x = 0.0f;
+		coordB.y = 0.0f;
+
+		coordC.x = 0.0f;
+		coordC.y = 0.0f;
+	}
+
+public:
+	bool rotate(float T)
+	{
+
+	}
+};
+
 class olcEngine3D : public olcConsoleGameEngine
 {
 public:
@@ -7,9 +38,15 @@ public:
 	{
 		m_sAppName = L"Pickled Engine";
 	}
+
+private:
+	triangle triangle1;
+
 public:
 	bool OnUserCreate() override
 	{
+		//triangle1.p = [0.0f, 0.0f, 1.0f, -2.0f, -1.0f, -2.0f];
+		
 		return true;
 	}
 
